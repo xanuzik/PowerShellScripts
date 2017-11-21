f91dcbc97dca07ffaa3308f4d4ce9163db98408d
xanuzik <33548047+xanuzik@users.noreply.github.com>
2017-11-21 22:30:42 +0800

    11.21
    further process of raw data to be added

---
+++ SystemAdmin/get-cpu-usage.ps1
@@ -0,0 +1,2 @@
+﻿#get the cpu usage of a specific process in a period of time
+(Get-Counter '\process($porcessname)\% processor time' -SampleInterval $interval -MaxSamples $times).countersamples.cookedvalue
