4240209ce33c2e9f6eafe4a14f6522656ab981b6
xanuzik <33548047+xanuzik@users.noreply.github.com>
2017-11-23 12:33:15 +0800

    11.23

--- SystemAdmin/get-cpu-usage.ps1
+++ SystemAdmin/get-cpu-usage.ps1
@@ -1,2 +1,17 @@
-﻿#get the cpu usage of a specific process in a period of time
-(Get-Counter '\process($porcessname)\% processor time' -SampleInterval $interval -MaxSamples $times).countersamples.cookedvalue
+﻿$sum=0
+
+#get proccessor time 
+$values = (get-counter '\process($processname)\% processor time' -SampleInterval 1 -MaxSamples $sampletime).countersamples.cookedvalue
+
+#get processor time sum
+foreach($value in $values)
+{
+$sum=$sum+$value
+}
+
+#get cpu numbers
+$cpuno = (Get-WmiObject -class win32_processor).numberoflogicalprocessors
+
+#get average processor time
+$ave = $sum/$cpuno
+echo "the average is $ave"
