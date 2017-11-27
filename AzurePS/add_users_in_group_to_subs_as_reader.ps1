1048de5ef27defd7067aa5ba334b8dd43b27af30
xanuzik <33548047+xanuzik@users.noreply.github.com>
2017-11-27 17:31:49 +0800

    11.27

---
+++ AzurePS/add_users_in_group_to_subs_as_reader.ps1
@@ -0,0 +1,24 @@
+﻿#the script is to add the users in a specific group to all subcriptions as reader
+#the script should be run by superadmin who has permissions of all subscriptions
+
+#$subids = (Get-AzureRmSubscription).SubscriptionId
+#$subids = get-content -Path D:\bbb.txt
+$admingroupid = (Get-AzureRmADGroup -SearchString azureadmin_cn).id.tostring()
+$adminnames = (Get-AzureRmADGroupMember -GroupObjectId $admingroupid).userprincipalname
+foreach($subid in $subids)
+{
+    Get-AzureRmSubscription -SubscriptionId $subid | Select-AzureRmSubscription
+    $subuers = (Get-AzureRmRoleAssignment).signinname
+    foreach($adminname in $adminnames)
+    {
+        $judge = $subuers.Contains($adminname)
+        if ($judge -eq "true")
+        {
+        }
+        else
+        {
+          $adminnameid = (Get-AzureRmADUser -UserPrincipalName $adminname).id.tostring()
+          New-AzureRmRoleAssignment -ObjectId $adminnameid -RoleDefinitionName reader -Scope /subscriptions/$subid  
+        }
+    }
+}
